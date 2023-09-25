f8464b975411595b11e2e929f70c98b267f85232
Alan-Grech295 <77152357+Alan-Grech295@users.noreply.github.com>
2023-09-25 17:25:30 +0200

    Basic event system

---
+++ Photon/src/Photon/Events/ApplicationEvent.h
@@ -0,0 +1,109 @@
+#pragma once
+#include "Event.h"
+
+#include <sstream>
+
+namespace Photon
+{
+	class PHOTON_API WindowCloseEvent : public Event
+	{
+	public:
+		WindowCloseEvent() {}
+
+		EVENT_CLASS_TYPE(WindowClose)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class PHOTON_API WindowResizeEvent : public Event
+	{
+	public:
+		WindowResizeEvent(uint32_t width, uint32_t height)
+			: m_Width(width), m_Height(height)
+		{}
+
+		inline uint32_t GetWidth() const { return m_Width; }
+		inline uint32_t GetHeight() const { return m_Height; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowResize)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+	private:
+		uint32_t m_Width, m_Height;
+	};
+
+	class PHOTON_API WindowFocusEvent : public Event
+	{
+	public:
+		WindowFocusEvent() {}
+
+		EVENT_CLASS_TYPE(WindowFocus)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class PHOTON_API WindowLostFocusEvent : public Event
+	{
+	public:
+		WindowLostFocusEvent() {}
+
+		EVENT_CLASS_TYPE(WindowLostFocus)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class PHOTON_API WindowMovedEvent : public Event
+	{
+	public:
+		WindowMovedEvent(uint32_t x, uint32_t y)
+			: m_X(x), m_Y(y)
+		{}
+
+		inline uint32_t GetX() const { return m_X; }
+		inline uint32_t GetY() const { return m_Y; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "WindowMovedEvent: " << m_X << ", " << m_Y;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowMoved)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+	private:
+		uint32_t m_X, m_Y;
+	};
+
+	class PHOTON_API AppTickEvent : public Event
+	{
+	public:
+		AppTickEvent() {}
+
+		EVENT_CLASS_TYPE(AppTick)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class PHOTON_API AppUpdateEvent : public Event
+	{
+	public:
+		AppUpdateEvent() {}
+
+		EVENT_CLASS_TYPE(AppUpdate)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class PHOTON_API AppRenderEvent : public Event
+	{
+	public:
+		AppRenderEvent() {}
+
+		EVENT_CLASS_TYPE(AppRender)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+}
---
+++ Photon/src/Photon/Events/KeyEvent.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "Event.h"
+
+#include <sstream>
+
+namespace Photon
+{
+	class PHOTON_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int keyCode)
+			: m_KeyCode(keyCode)
+		{}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << GetName() << ": " << m_KeyCode;
+			return ss.str();
+		}
+
+	protected:
+		int m_KeyCode;
+	};
+
+	class PHOTON_API KeyPressedEvent : public KeyEvent
+	{
+	public: 
+		KeyPressedEvent(int keyCode, int repeatCount)
+			: KeyEvent(keyCode), m_RepeatCount(repeatCount)
+		{}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << ToString() << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+	private:
+		int m_RepeatCount;
+	};
+
+	class PHOTON_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keyCode)
+			: KeyEvent(keyCode)
+		{}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+}
